86ffdb642826468d17a5d6a2a67d1f3a23961c8b
ItaloAraujodev <italoarauju2019@gmail.com>
2024-04-16 11:21:55 -0300

    refactor: Refactor code structure, update dependencies, and add NestJS config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { PrismaService } from "./prisma/prisma.service";
 import { CreateAccountController } from "./controllers/create-account.controller";
+import { envSchema } from "./env";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true, // isGlobal: true para que o módulo seja global e não seja necessário importá-lo em outros módulos
+    }),
+  ], // Configuração do dotenv
   controllers: [CreateAccountController], // Tudo que tem requisição http
   providers: [PrismaService], // Tudo que não tem requisição http
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { Env } from "./env";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false, // Desabilita o logger padrão do NestJS, pq mais pra frente vai ficar muito logger no console
   });
-  await app.listen(3333);
+  const configService: ConfigService<Env, true> = app.get(ConfigService); // Pega o serviço de configuração
+  const port = configService.get("PORT", { infer: true }); // Pega a porta do arquivo .env
+
+  await app.listen(port);
 }
 bootstrap();
