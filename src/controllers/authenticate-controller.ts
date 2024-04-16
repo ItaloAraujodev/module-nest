ce9028c30b7740ec86f1b41a402805c5054e22f7
ItaloAraujodev <italoarauju2019@gmail.com>
2024-04-16 14:59:11 -0300

    feat: Refactor AuthenticateController to use ZodValidationPipe and PrismaService

--- src/controllers/authenticate-controller.ts
+++ src/controllers/authenticate-controller.ts
@@ -1,13 +1,55 @@
-import { Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { compare } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const autheticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type AutheticateBodySchema = z.infer<typeof autheticateBodySchema>;
 
 @Controller("/sessions")
+@UsePipes(new ZodValidationPipe(autheticateBodySchema))
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   @Post()
-  async handle() {
-    const token = this.jwt.sign({ sub: "123" });
-    return token;
+  async handle(@Body() body: AutheticateBodySchema) {
+    const { email, password } = body;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException("User not found");
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException("User not found");
+    }
+
+    const acessToken = this.jwt.sign({ sub: user.id });
+
+    return {
+      acessToken,
+    };
   }
 }
